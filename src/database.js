f57a94f773ff8df6c7f6d41d1b8e04037bf42c19
nduy79 <ngocduy0997@gmail.com>
2019-06-19 15:26:34 +0700

    commit: add database setup

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/pizza-orderring';
+
+mongoose.connect(url, {useNewUrlParser: true});
+let db = mongoose. connection;
+db.on('error', console.error.bind(console, 'connection to DB error'));
+db.once('open', function (){
+    console.log('Connected to server successfull !!! ');
+    module.exports = db;
+} );
+
