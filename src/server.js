0fb2edb75f6aa5af2ce98003624bbb5778a12ce0
nduy79 <ngocduy0997@gmail.com>
2019-06-25 10:50:39 +0700

    commit:fix server

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 async function StartServer() {
   await database.connect;
   const server = new Hapi.server({
-    host: process.env.HOST || config.server.HOST||'0.0.0.0',
+    // host: process.env.HOST || config.server.HOST||'0.0.0.0',
     port: process.env.PORT || 3000
   });
   await server.register([
