5155772868587f3468615d3c24f6612137329774
nduy79 <ngocduy0997@gmail.com>
2019-07-02 14:28:29 +0700

    commit: fix deploy1

--- src/database/init.js
+++ src/database/init.js
@@ -3,7 +3,7 @@ const config = require('../configuration/envConfiguration');
 
 
 
-const url = config.database.urlCloudgit 
+const url = config.database.urlCloud
 
 mongoose.connect(url, {
     useNewUrlParser: true
