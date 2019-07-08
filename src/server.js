a425a03db0d2a8c85f70df946990d7c9b310d14c
nduy79 <ngocduy0997@gmail.com>
2019-07-08 13:46:50 +0700

    commit: deploy on heroku1

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 async function StartServer() {
   await Database.connect;
   const server = new Hapi.server({
-    host: process.env.HOST,
+    // host: process.env.HOST,
     port: process.env.PORT || 3646,
     query: {
       parser : (query)=> Qs.parse(query)
